64e72e09c97e5dbf93adef03d7c085a88f9f845d
SANNY200 <sannyofficial2001@gmail.com>
2024-11-24 04:15:16 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,5 +1,5 @@
 const config = require('../config')
-const {cmd , commands} = require('../command')
+const {cmd} = require('../command')
 const {sleep} = require('../lib/functions')
 
 cmd({
@@ -9,14 +9,15 @@ cmd({
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!isOwner)return
-const {exec} = require("child_process")
-reply("restarting...")
-await sleep(1500)
-exec("pm2 restart all")
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
+    try{
+        if(!isOwner) return reply('This command is only for the owner!');
+        
+        const {exec} = require("child_process")
+        reply("🔄 Restarting bot...")
+        await sleep(1500)
+        exec("pm2 restart all")
+    }catch(e){
+        console.log(e)
+        reply(`${e}`)
+    }
 })
